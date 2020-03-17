84d449bcf0dad323e099bc5e13e37a1878b9c5f1
kira231 <49626803+kira231@users.noreply.github.com>
2020-03-17 21:09:04 +0800

    分流页面添加社区统计

---
+++ src/components/Statistics.vue
@@ -0,0 +1,58 @@
+<template>
+  <header>
+    <div class="user">
+      <img style="height:40px;vertical-align: middle;"
+           src="../assets/images/用户.png">
+    </div>
+    <div class="userinfo">
+      <div class="username">测试用户1 </div>
+    </div>
+    <div class="right_icon">
+      <img style="height:30px;vertical-align: middle;"
+           src="../assets/images/设置.png">
+    </div>
+  </header>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+header {
+  color: white;
+  height: 80px;
+  background: blue;
+}
+.user {
+  vertical-align: middle;
+  height: 100%;
+  width: 20%;
+  float: left;
+  line-height: 80px;
+}
+.userinfo {
+  height: 80px;
+  float: left;
+  width: 50%;
+}
+.username {
+  display: block;
+  float: left;
+  margin-block-start: 25px;
+  margin-block-end: 20px;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+}
+.right_icon {
+  font-size: 8px;
+  float: left;
+  width: 26%;
+  line-height: 80px;
+  text-align: right;
+  padding-right: 4%;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -9,48 +9,53 @@ import PatForgetpwd from '@/components/PatForgetpwd'
 import PatRegister from '@/components/PatRegister'
 import DrForgetpwd from '@/components/DrForgetpwd'
 import DrRegister from '@/components/DrRegister'
+import Statistics from '@/components/Statistics'
 Vue.use(Router)
 
 export default new Router({
   routes: [{
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/doctor',
-      name: 'doctor',
-      component: doctor
-    },
-    {
-      path: '/patient',
-      name: 'patient',
-      component: patient
-    }, {
-      path: '/Split',
-      name: 'Split',
-      component: Split
-    }, {
-      path: '/Train',
-      name: 'Train',
-      component: Train
-    }, {
-      path: '/PatForgetpwd',
-      name: 'PatForgetpwd',
-      component:  PatForgetpwd
-    }, {
-      path: '/PatRegister',
-      name: 'PatRegister',
-      component:  PatRegister
-    },
-    {
-      path: '/DrForgetpwd',
-      name: 'DrForgetpwd',
-      component:  DrForgetpwd
-    }, {
-      path: '/DrRegister',
-      name: 'DrRegister',
-      component: DrRegister
-    }
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  },
+  {
+    path: '/doctor',
+    name: 'doctor',
+    component: doctor
+  },
+  {
+    path: '/patient',
+    name: 'patient',
+    component: patient
+  }, {
+    path: '/Split',
+    name: 'Split',
+    component: Split
+  }, {
+    path: '/Train',
+    name: 'Train',
+    component: Train
+  }, {
+    path: '/PatForgetpwd',
+    name: 'PatForgetpwd',
+    component: PatForgetpwd
+  }, {
+    path: '/PatRegister',
+    name: 'PatRegister',
+    component: PatRegister
+  },
+  {
+    path: '/DrForgetpwd',
+    name: 'DrForgetpwd',
+    component: DrForgetpwd
+  }, {
+    path: '/DrRegister',
+    name: 'DrRegister',
+    component: DrRegister
+  }, {
+    path: '/Statistics',
+    name: 'Statistics',
+    component: Statistics
+  },
   ]
 })
