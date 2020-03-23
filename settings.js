2c5b850943716b3e52f287e8aec1d8988fd1c0d4
zyhxym <zyhxym@gmail.com>
2020-03-23 11:03:38 +0800

    no message

--- settings.js
+++ settings.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 // axios.defaults.baseURL = "http://101.132.140.25:3000";
 // axios.defaults.baseURL = "http://47.98.252.131:3001";
-axios.defaults.baseURL = "http://118.178.56.71:3001";
+axios.defaults.baseURL = "http://frznflxt.zhejianglab.com:3001";
 
 // axios.defaults.baseURL = "http://frznflxt.zhejianglab.com:8001";
